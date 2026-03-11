e146ee59244bcef6e8a096ff3d73d184a2e64c89
Marcos-gjr <m.goncalves.oficial@gmail.com>
2026-03-11 20:11:20 -0300

    feat: alteração na criação do nginx para funcionar em conjunto com futuro data-dashboard

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,6 +56,14 @@ resource "aws_security_group" "permitir_web_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Streamlit Dashboard"
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
